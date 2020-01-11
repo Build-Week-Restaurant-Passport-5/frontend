9b56411ff476ecf007c743eac62ac3aff929e6a1
Pete James <petejames@petes-air.lan>
2020-01-11 12:36:49 -0800

    more styling

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-import { Card, Icon, Image } from "semantic-ui-react";
+import { Card, Icon, Image, Header } from "semantic-ui-react";
 
 import RestaurantCard from "./RestaurantCard";
 
@@ -26,7 +26,7 @@ const RestaurantList = props => {
   useEffect(getRestaurants, []);
 
   return (
-    <Card.Group itemsPerRow={3} className='restaurant-list' stackable>
+    <Card.Group itemsPerRow={2} className='restaurant-list' stackable>
      
       <Link to='/add'><Icon size='large' circular name='add' link /></Link>
       
@@ -36,7 +36,7 @@ const RestaurantList = props => {
             src='https://raw.githubusercontent.com/Build-Week-Restaurant-Passport-5/frontend/master/design_files/restaurant-passport.png'
            
           />
-          <Image size='medium' src='https://35ht6t2ynx0p1ztf961h81r1-wpengine.netdna-ssl.com/wp-content/uploads/2015/01/Blank-image.jpg' />
+      <Header floated='right' size='huge'>A home for all your foodie aspirations</Header>
       {restaurants.map(item => (
         <RestaurantCard key={item.id} restaurant={item} />
       ))}
