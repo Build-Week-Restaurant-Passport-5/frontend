4b5fbee8cc440a839e5fd8e6a71f3955460cfa62
Pete James <petejames@petes-air.lan>
2020-01-13 17:43:58 -0800

    added loading screen on details page so that async data loads before render

--- src/components/RestaurantDetails.js
+++ src/components/RestaurantDetails.js
@@ -5,6 +5,9 @@ import { Button, Card, Image, Transition, Icon, Rating } from "semantic-ui-react
 
 const RestaurantDetails = ({ match }) => {
     const [details, setDetails] = useState({});
+    const [loadState, setLoadState] = useState('waiting');
+
+
     const getRestaurants = () => {
         axios
             .get(
@@ -13,29 +16,47 @@ const RestaurantDetails = ({ match }) => {
             .then(response => {
                 console.log('response', response.data);
                 setDetails(response.data);
+                setLoadState('done!');
+
             })
             .catch(error => {
                 console.error("Server Error", error);
             });
     };
     useEffect(getRestaurants, []);
-    console.log("Item:", details);
-    var rating = details.myRating;
 
-    return <div>
-    <Card fluid>
+    useEffect(() => {
+        console.log('status has changed', loadState);
+    
+    }, [loadState]);
+
+
+    var rating = details.myRating;
+    console.log("Item:", details);
+    console.log('rating type is', typeof(rating));
+
+    if (loadState === 'waiting'){
+        return <div><h1>Loading......</h1></div>
+    } else {
+
+
+            return <div>
+        <Card fluid>
         <Card.Content>
-        <Card.Header>        <Icon name='food' /> {details.restaurantName}</Card.Header>
+        <Card.Header><Icon name='food' /> {details.restaurantName}</Card.Header>
         <Card.Meta>{details.streetAddress}</Card.Meta>
         <Card.Meta>{details.city}</Card.Meta>
         <Card.Meta>{details.zipcode}</Card.Meta>
         <Card.Meta>{details.phoneNumber}</Card.Meta>
         <Card.Meta><a href={details.websiteURL}>{details.websiteURL}</a> </Card.Meta>
-<Card.Meta><Rating defaultRating={ details.myRating } maxRating={5} disabled /></Card.Meta>
+        <Card.Meta><Rating defaultRating={details.myRating} maxRating={5} disabled /></Card.Meta>
         <Card.Meta>{details.notes}</Card.Meta>
         </Card.Content>
     </Card>
     </div>;
-};
+}
+        };
+
+
 
 export default RestaurantDetails;
